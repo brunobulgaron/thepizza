a4bf1866e8f849561a9a1335c2b282841a2ad35d
brunobulgaron <brunohbulgaron@gmail.com>
2021-07-14 14:28:31 -0300

    Added Header and Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <Header />
+      <main>
+        <h1>Welcome to ThePizza!</h1>
+      </main>
+      <Footer />
     </>
   )
 }
