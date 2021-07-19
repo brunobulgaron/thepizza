867885b69c47c4b7fd8a052526d01ce87a694ea4
brunobulgaron <brunohbulgaron@gmail.com>
2021-07-18 22:47:14 -0300

    Added ShippingPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import CartPage from "./pages/CartPage/CartPage"
 import LoginPage from "./pages/LoginPage/LoginPage"
 import RegisterPage from "./pages/RegisterPage/RegisterPage"
 import ProfilePage from "./pages/ProfilePage/ProfilePage"
+import ShippingPage from "./pages/ShippingPage/ShippingPage"
 import { Container } from "react-bootstrap"
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegisterPage} />
           <Route path="/profile" component={ProfilePage} />
+          <Route path="/shipping" component={ShippingPage} />
         </Container>
       </main>
       <Footer />
